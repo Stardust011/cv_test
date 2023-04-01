0e01222ae8f8f5323b7d4997e1d7480bb095b719
Stardust011 <95744877+Stardust011@users.noreply.github.com>
2023-04-01 18:18:19 +0800

    Dev night (#5)
    * 串口
    
    * 自启动
    
    * #4 修复

--- Inc/config.h
+++ Inc/config.h
@@ -12,6 +12,7 @@
 #define IMAGE_WIDTH 3264
 #define IMAGE_HEIGHT 2448
 #define FPS 30
+#define VIDEO "/dev/video1"
 #define SERIAL_PORT "/dev/ttyUSB0"
 #define SERIAL_BAUDRATE 115200
 
--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
-sudo ln -s /dev/video0 /dev/video1
+VIDEO_FILE=/usr/local/bin/opencv_version
+if [ test -f "$VIDEO_FILE" ]; then
+    echo "Find video1"
+    else sudo ln -s /dev/video0 /dev/video1
+fi
+
 v4l2-ctl -d /dev/video1 -c exposure_auto=1
 v4l2-ctl -d /dev/video1 -c exposure_absolute=1
-sudo ./armour1
+
+sudo /etc/cv_run/cv_run
